b29d702cddc6d8fd40c52e6b6351f79457103acc
sudeep <sudeepchalla1@gmail.com>
2026-01-07 19:33:42 +0530

    fix:fixed build version

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,18 +1,9 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-        app.handle().plugin(tauri_plugin_shell::init())?;
-        app.handle().plugin(tauri_plugin_sql::Builder::default().build())?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_sql::Builder::default().build())
+        .plugin(tauri_plugin_log::Builder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
